9d17925f7547a1e856563f76f28e21161028d15c
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-09-02 11:08:12 -0700

    add axios and exercisedb api

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import './styles/App.scss';
 import Button from '@mui/material/Button';
+const axios = require("axios");
+
+const options = {
+  method: 'GET',
+  url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
+  headers: {
+    'X-RapidAPI-Key': '952f636cd0msha0564ecc5a311f8p1c3b0ajsn950cbe068bb5',
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+  }
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
 
 function App() {
   return (
     <div className="App">
       <h1>Frame Fitness</h1>
-      <Button variant="contained">MUI button</Button>
+      <Button className='button' variant="contained">MUI button</Button>
     </div>
   );
 }
