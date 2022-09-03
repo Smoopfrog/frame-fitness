521166929ee80f757aa7a8c6bb5ef2d8f6bb3c1a
Live session user <jeandrev1414@gmail.com>
2022-09-03 14:42:40 -0600

    Add Login and Signup

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,32 +9,37 @@ const Navbar = () => {
       backgroundColor= '#FF9700'
       direction='row'
       justifyContent='space-between'
+      px='20px'
+      sx={{ gap: {sm: '120px', xs: '40px'}}}
     >
-      <Link to='/' style={{textDecoration: 'none', color: 'white'}}>
+      <Link to='/' style={{textDecoration: 'none', color: 'white', height: '70px'}}>
         <Stack 
           direction='row' 
-          alignItems='flex-start'
+          alignItems='center'
           textAlign='right'>
           
           <img src={Dumbbell} alt="logo" 
-          style={{width: '50px', marginLeft: '20px', marginTop: '10px'}} />
+          style={{width: '50px', marginLeft: '20px', marginBottom: '10px'}} />
+          
           <h1>&nbsp;Frame Fitness</h1>
+
+          <a href="#exercises" style={{textDecoration: 'none', color: 'white', marginLeft: '30px', fontSize:'26px'}}>Exercises</a>
         </Stack>
 
       </Link>
-
+     
       <Stack 
         direction='row'
-        gap='30px'
+        gap='20px'
         fontSize='26px'
         alignItems='center'
         marginRight='20px'
+        marginTop='10px'
       >
-        <Link to="/"
-          style={{textDecoration: 'none', fontWeight: 'bolder', color: 'white'}}>Home</Link>
-
-        <a href="#exercises" style={{textDecoration: 'none', color: 'white'}}>Exercises</a>
+        <a variant="text" href="#login" style={{color: 'white', textDecoration: 'none'}}>Login</a>
+        <a variant="text" href="#signup" style={{color: 'white', textDecoration: 'none'}}>Sign up</a>
       </Stack>
+
 
     </Stack>
   )
