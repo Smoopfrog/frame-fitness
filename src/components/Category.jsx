a0121187c6d8822cc71d1de4b330087b9cc0a42e
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-09-02 20:11:23 -0700

    change styling for categories

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,10 +1,10 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 const Category = (props) => {
 
   return (
     <Box className="category">
-      <h1>{props.name}</h1>
+      <Typography fontSize="24px" fontWeight="bold" color="black" textTransform="capitalize" align="center">{props.name}</Typography>
       <img src="https://icon-library.com/images/bicep-icon/bicep-icon-16.jpg"/>
     </Box>
   )
