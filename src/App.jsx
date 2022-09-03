7d67f11c74e0003693fa3670bc6a9fd7c1d32e3f
Live session user <jeandrev1414@gmail.com>
2022-09-03 12:33:31 -0600

    Add styling to Box

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './styles/App.scss';
 import Navbar from './components/NavBar';
-import Home from './pages/Home'
+import Home from './pages/Home';
 import { Box } from '@mui/material';
 import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <Box>
+    <Box width='600px' sx={{ width: {xl: '1200px', fontFamily: 'Calibre', backgroundColor: 'white' }}} m='auto' >
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
