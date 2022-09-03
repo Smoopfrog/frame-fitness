801a265a03957641de2c8cb6ce991da2776f6b59
Live session user <jeandrev1414@gmail.com>
2022-09-03 17:22:13 -0600

    Import HeroBanner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,12 @@ import { Box } from '@mui/system'
 import React from 'react'
 
 import CategoriesBar from '../components/CategoriesBar'
+import { HeroBanner } from '../components/HeroBanner'
 
 const Home = () => {
   return (
     <Box>
+      <HeroBanner />
      { /*<CategoriesBar /> */}
     </Box>
   )
