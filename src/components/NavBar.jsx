17fcb88695a498ba496068e51a297bc2b8eb238b
Live session user <jeandrev1414@gmail.com>
2022-09-02 23:02:33 -0600

    Add Logo, home, and exercises to navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,10 +5,37 @@ import Dumbbell from '../assets/dumbbell.png';
 
 const Navbar = () => {
   return (
-    <Stack>
-      <Link to='/'>
-        <img src={Dumbbell} alt="logo" className='logo' />
+    <Stack 
+      backgroundColor= '#FF9700'
+      direction='row'
+      justifyContent='space-between'
+    >
+      <Link to='/' style={{textDecoration: 'none', color: 'white'}}>
+        <Stack 
+          direction='row' 
+          alignItems='flex-start'
+          textAlign='right'>
+          
+          <img src={Dumbbell} alt="logo" 
+          style={{width: '50px', marginLeft: '20px', marginTop: '10px'}} />
+          <h1>&nbsp;Frame Fitness</h1>
+        </Stack>
+
       </Link>
+
+      <Stack 
+        direction='row'
+        gap='30px'
+        fontSize='26px'
+        alignItems='center'
+        marginRight='20px'
+      >
+        <Link to="/"
+          style={{textDecoration: 'none', fontWeight: 'bolder', color: 'white'}}>Home</Link>
+
+        <a href="#exercises" style={{textDecoration: 'none', color: 'white'}}>Exercises</a>
+      </Stack>
+
     </Stack>
   )
 }
