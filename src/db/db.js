d2197708eb820dcce3cb5032e77e42793a4f7176
Live session user <jeandrev1414@gmail.com>
2022-09-05 16:27:18 -0600

    Reorganize

--- src/db/db.js
+++ src/db/db.js
@@ -3,9 +3,9 @@ require('../../.env').config();
 
 const pool = new Pool({
   database: process.env.DATABASE_NAME,
+  host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
-  host: process.env.DATABASE_HOST,
   port: process.env.DATABASE_PORT
 });
 
