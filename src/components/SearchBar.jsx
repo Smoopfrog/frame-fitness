c642999aaa07e2a5c7cec33500461e22936f5943
Live session user <jeandrev1414@gmail.com>
2022-09-06 16:21:33 -0600

    Style search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { TextField, Box, Button, Typography } from "@mui/material";
 import { fetchData, exerciseOptions } from "../utils/fetchData";
+import '../styles/App.scss';
 
 const SearchBar = ({ setExercises }) => {
   const [search, setSearch] = useState('');
@@ -28,13 +29,13 @@ const SearchBar = ({ setExercises }) => {
   };
 
   return (
-    <Box className="search-bar">
-      <Typography>
-        Find some new exercises
+    <Box className="search-bar" >
+      <Typography sx={{color: '#FF9700', fontFamily: 'Calibre', fontSize: '40px', lineHeight: 'normal', mb: '10px', color: '#00A5B8'}}>
+        Explore a Catelogue of 1300+ Exercises
       </Typography>
       <TextField
         height="76px"
-        sx={{ input: { fontWeight: '700', border: 'none', borderRadius: '4px' }, width: { lg: '1170px', xs: '350px' }, backgroundColor: '#fff', borderRadius: '40px' }}
+        sx={{ input: { fontWeight: '700', border: 'none', borderRadius: '4px' }, width: { lg: '500px', xs: '300px' }, backgroundColor: '#fff', borderRadius: '40px' }}
         value={search}
         onChange={(e) => setSearch(e.target.value.toLowerCase())}
         placeholder="Search Exercises"
@@ -44,6 +45,8 @@ const SearchBar = ({ setExercises }) => {
       </TextField>
       <Button className="search-btn"
         onClick={searchFunction}
+        sx={{'&:hover': {backgroundColor: '#fff', color: '#FF9700', border: '2px solid #FF9700', padding: '14px 6px'
+        }}}
       >
         Search
       </Button>
