7296f331f0acd516cea6dee47f6ed1632155d813
Live session user <jeandrev1414@gmail.com>
2022-09-06 14:23:08 -0600

    Remove firstname, lastname, and email and replace with username

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -1,58 +1,26 @@
-import React, { useContext, useState } from 'react'
+import React from 'react'
 import { Grid, Paper, Button, Typography } from '@mui/material'
 import { TextField } from '@mui/material'
 import { Formik, Form, Field, ErrorMessage } from 'formik'
 import * as Yup from 'yup';
-import { AccountContext } from './AccountContext';
-import { useNavigate } from "react-router";
 
 const SignupForm = () => {
-  const [error, setError] = useState(null);
-  const setUser = useContext(AccountContext);
-  const navigate = useNavigate();
+
     const initialValues = {
-        firstName: '',
-        lastName: '',
-        email: '',
+        username: '',
         password: '',
         confirmPassword:''
     }
     const validationSchema = Yup.object().shape({
-      firstName: Yup.string().min(1, "Too short").required("Required"),
-      lastName: Yup.string().min(1, "Too short").required("Required"),
-      email: Yup.string().email("Enter a valid email").required("Required"),
-      password: Yup.string().min(8, "Password must be a minimum of 8 characters").required("Required"),
+      username: Yup.string().min(6, "Too short").required("Required"),
+      password: Yup.string().min(6, "Password must be a minimum of 6 characters").required("Required"),
       confirmPassword:Yup.string().oneOf([Yup.ref('password')],"Passwords do not match").required('Required')
     })
     const onSubmit = (values, props) => {
-        const vals = { ...values };
-        alert(JSON.stringify(values), null, 2)
+        
+        console.log(JSON.stringify(values), null, 2)
         props.resetForm()
-        fetch('/auth/signup', {
-          method: "POST",
-          credentials: "include",
-          headers: {
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify(vals),
-        }).catch(err => {
-          return;
-        })
-        .then(res => {
-          if (!res || !res.ok || res.status >= 400) {
-            return;
-          }
-          return res.json();
-        })
-        .then(data => {
-          if (!data) return;
-          setUser({...data});
-          if(data.status) {
-            setError(data.status)
-          } else if (data.loggedIn) {
-            navigate('/')
-          }
-        })
+        
     }
     return (
     <Grid >
@@ -64,19 +32,9 @@ const SignupForm = () => {
             {(props) => (
           <Form noValidate>
             <Grid mb='10px'>
-              <Field as={TextField} name='firstName' label='First Name' fullWidth
-                  error={props.errors.name && props.touched.name}
-                  helperText={<ErrorMessage name='firstName' />} required />
-            </Grid>
-            <Grid mb='10px'>
-              <Field as={TextField} name='lastName' label='Last Name' fullWidth
-                  error={props.errors.name && props.touched.name}
-                  helperText={<ErrorMessage name='lastName' />} required />
-            </Grid>
-            <Grid mb='10px'>
-              <Field as={TextField} name='email' label='Email' fullWidth
-                  error={props.errors.email && props.touched.email}
-                  helperText={<ErrorMessage name='email' />} required />
+              <Field as={TextField} name='username' label='Username' fullWidth
+                  error={props.errors.username && props.touched.username}
+                  helperText={<ErrorMessage name='username' />} required />
             </Grid>
             <Grid mb='10px'>
               <Field as={TextField} name='password' label='Password' type='password' fullWidth
