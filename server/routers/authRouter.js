fcf99baab95e703771b5faeb1ddb3cc2794493f7
Live session user <jeandrev1414@gmail.com>
2022-09-06 14:25:38 -0600

    Add routing for signup and login

--- server/routers/authRouter.js
+++ server/routers/authRouter.js
@@ -7,42 +7,44 @@ const validateLogin = require('../controllers/validateLogin');
 
 
 // Signup validation
-router.post('/auth/signup', async (req, res) => {
+router.post('/signup', async (req, res) => {
   validateSignup(req, res);
 
-  const existingUser = await pool.query("SELECT email from users WHERE email=$1", [req.body.email])
+  const existingUser = await pool.query("SELECT username from users WHERE username=$1", [req.body.username])
+
+  console.log(existingUser)
 
   if(existingUser.rowCount === 0) {
     // signup user
     const hashedPassword = await bcrypt.hash(req.body.password, 20)
     const queryNewUser = await pool.query(
-      "INSERT INTO users (first_name, last_name, email, passhash) VALUES ($1, $2, $3, $4) RETURNING id, email",
+      "INSERT INTO users (username, passhash) VALUES ($1, $2) RETURNING id, username",
       [req.body.username, hashedPassword]);
     req.session.user = {
-      email: req.body.email,
+      username: req.body.username,
       id: queryNewUser.rows[0].id
     };
-    res.json({loggedIn: true, email: req.body.email})
+    res.json({loggedIn: true, username: req.body.username})
   } else {
-    res.json({loggedIn: false, status: "Email already in use"})
+    res.json({loggedIn: false, status: "Username already in use"})
   }
 });
 
 // Login validation
-router.post('/auth/login', async (req, res) => {
+router.post('/login', async (req, res) => {
   validateLogin(req, res);
 
-  const loginInfo = await pool.query("SELECT id, email, passhash FROM users WHERE users.email=$3 AND users.passhash=$4", [req.body.email]);
+  const loginInfo = await pool.query("SELECT id, username, passhash FROM users WHERE users.username=$1 AND users.passhash=$2", [req.body.username]);
 
   if (loginInfo.rowCount > 0) {
     const samePassword = await bcrypt.compare(req.body.password, loginInfo.rows[0].passhash);
     
     if(samePassword) {
         req.session.user = {
-          email: req.body.email,
+          username: req.body.username,
           id: loginInfo.rows[0].id
         }
-        res.json({loggedIn: true, email: req.body.email})
+        res.json({loggedIn: true, username: req.body.username})
     } else {
         res.json({loggedIn: false, status: "Wrong username or password"})
     }
