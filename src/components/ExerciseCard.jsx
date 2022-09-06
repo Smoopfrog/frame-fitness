737073e876496a8feb3ad0b107fac021e2a47fe2
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-09-05 17:59:49 -0700

    refactor styling

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -3,11 +3,11 @@ import React from "react";
 
 const ExerciseCard = ({exercise}) => {
   return ( 
-    <Box sx={{border: "4px solid #E5661F", borderRadius: "20px", backgroundColor: '#E5661F'}}>
-      <Typography textAlign="center" color="white" fontWeight="bold" sx={{ fontSize: { lg: '24px', xs: '20px' } }} mt="11px" pb="10px" textTransform="capitalize">
+    <Box className="exercise-card">
+      <Typography className="exercise-card-name" sx={{ fontSize: { lg: '24px', xs: '20px' } }} >
         {exercise.name}
       </Typography>
-        <img margin="inherit" src={exercise.gifUrl} alt={exercise.name} loading="lazy" />
+      <img margin="inherit" src={exercise.gifUrl} alt={exercise.name} loading="lazy" />
       <Stack direction="row">
         <Button className="exercise-card-btn">
           {exercise.bodyPart}
