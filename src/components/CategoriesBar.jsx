38c33a9b7eec0fc870a868376930725396e0a7ab
Live session user <jeandrev1414@gmail.com>
2022-09-06 21:16:22 -0600

    Restyled Toogle buttons

--- src/components/CategoriesBar.jsx
+++ src/components/CategoriesBar.jsx
@@ -1,7 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { Typography, ToggleButton, ToggleButtonGroup } from '@mui/material';
-import { ScrollMenu } from 'react-horizontal-scrolling-menu';
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
 import { exerciseOptions, fetchData } from '../utils/fetchData';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import '../styles/App.scss';
+import { borderBottom } from '@mui/system';
+
+const LeftArrow = () => {
+  const { scrollPrev } = useContext(VisibilityContext);
+
+  return (
+    <Typography onClick={() => scrollPrev()} className="left-arrow">
+      <ArrowBackIcon fontSize="large" />
+    </Typography>
+  );
+};
+  const RightArrow = () => {
+    const { scrollNext } = useContext(VisibilityContext);
+  
+    return (
+      <Typography onClick={() => scrollNext()} className="right-arrow">
+        <ArrowForwardIcon fontSize="large" />
+      </Typography>
+    );
+  };
 
 const CategoriesBar = ({ category, setCategory, setExercises }) => {
   const categories = ['all', 'back', 'cardio', 'chest', 'lower arms', 'lower legs', 'neck', 'shoulders', 'upper arms', 'upper legs', 'waist']
@@ -29,26 +52,28 @@ const CategoriesBar = ({ category, setCategory, setExercises }) => {
         id={index}
         name={bodyPart}
         value={bodyPart}
+        sx={{background:'rgba(0, 0, 0, 0.4)', '&:hover': { background:'rgba(0, 0, 0, 0.4)'}}}
         onClick={() => setCategory(bodyPart)}
       >
-        <Typography fontSize="24px" fontWeight="bold" color="black" textTransform="uppercase" align="center">{bodyPart}</Typography>
+        <Typography fontSize="24px" fontWeight="bold" color="#FFF" textTransform="uppercase" align="center">{bodyPart}</Typography>
       </ToggleButton>
     )
   });
 
   return (
-    <ScrollMenu >
-      <ToggleButtonGroup
-        value={selected}
-        exclusive
-        onChange={handleSelected}
-        direction="row"
-        justifyContent="center"
-        alignItems="center"
-        spacing={2}
-      >
-        {bodyParts}
-      </ToggleButtonGroup>
+    <ScrollMenu 
+      LeftArrow={LeftArrow} 
+      RightArrow={RightArrow}
+      value={selected}
+      exclusive
+      onChange={handleSelected}
+      direction="row"
+      justifyContent="center"
+      alignItems="center"
+      spacing={2}
+    >
+      {bodyParts}
+      
     </ScrollMenu>
   )
 };
