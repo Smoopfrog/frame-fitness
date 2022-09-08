00d79936ba528f64c96bbba8d3b8866e08a10e3c
Live session user <jeandrev1414@gmail.com>
2022-09-08 12:50:32 -0600

    CHange size of font for slogan

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,14 +1,14 @@
 import { Box, Button, Typography } from '@mui/material';
 import { Stack } from '@mui/system';
 import React from 'react';
-import Banner from '../assets/banner3.jpg';
+import Banner from '../assets/banner.jpg';
 import '../styles/App.scss';
 
 export const HeroBanner = () => {
   return (
     <Stack align='center' >
       <Typography color='#FF9700' fontWeight='bolder' 
-        sx={{fontSize: {lg: '60px', md: '40px', xs: '30px'}}}
+        sx={{fontSize: {lg: '60px', md: '45px', xs: '35px'}}}
         width='100vw' fontFamily='Calibre' m='150px 0' backgroundColor='white' pt='10px' mb='0'>
         Lift, Breathe, Build Your Frame
       </Typography>
