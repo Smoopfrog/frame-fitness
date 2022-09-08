78a5d2854d99c7bb38531cc19a368691ff37fda6
Live session user <jeandrev1414@gmail.com>
2022-09-08 13:58:32 -0600

    Add and import Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/NavBar';
 import Home from './pages/Home';
 import { Box } from '@mui/material';
 import { Route, Routes } from 'react-router-dom';
+import Footer from './components/Footer';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
       </Routes>
+      <Footer />
     </Box>
   )
 }
