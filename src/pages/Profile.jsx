8e160a1490246c7a15eab5abe64c89c0d0bb9e8f
Live session user <jeandrev1414@gmail.com>
2022-09-08 22:08:00 -0600

    Add profile page with mock exercise card

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,92 @@
+import { Box, Typography, Button, ButtonGroup } from '@mui/material'
+import { Stack } from '@mui/system';
+import React, { useState } from 'react';
+import '../styles/App.scss';
+import All from '../assets/all.jpg';
+import DeleteIcon from '@mui/icons-material/Delete';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+
+const Profile = () => {
+
+  const [counter, setCounter] = useState(0);
+  const [counterRep, setCounterRep] = useState(0);
+
+  return (
+    <Stack className='profile' spacing={5}>
+      <Typography 
+        sx={{ fontSize: { lg: '60px', md: '50px', sm: '40px', xs: '30px'}, color: '#00A5B8'}}
+      >My Profile
+      </Typography>
+
+      <Stack direction='row' spacing={5 } >
+
+        <Stack className="profile-card" 
+          sx={{'&:hover': {boxShadow: '0 0 3px 5px #FF9700'}, objectPosition: 'center center'}}
+        >
+          <Typography className="profile-card-name" 
+              sx={{ fontSize: { lg: '24px', xs: '20px' } }} >
+            Exercise 
+          </Typography>
+
+          <img src={All} alt='photo' margin="inherit" style={{width: '200px', display: 'flex', justifyContent: 'center', alignItems: 'center'}}/>
+
+          <Stack direction="row">
+
+            <Box className="profile-card-btn" >
+              Body part
+            </Box>
+
+            <Box className="profile-card-btn">
+              equipment
+            </Box>
+
+            <Button className="profile-card-delete-btn" 
+                sx={{'&:hover': {background: 'white', color: 'red', border: '2px solid red', p: '4px 6px', backgroundColor: 'white'}}}>
+              <DeleteIcon fontSize="large"/>
+            </Button>
+
+          </Stack>
+
+        </Stack> 
+          <Stack spacing={5} className='quantity-stack' >
+            <Box >
+              <Typography className='quantity-name' >Sets</Typography>
+              <ButtonGroup size="large" aria-label="outlined button group" >
+
+                <Button className='quantity-btn'  onClick={()=> 
+                {setCounter(counter + 1)}}><AddIcon /></Button>
+
+                {<Box className='quantity-counter' >{counter}</Box>}
+
+                {<Button className='quantity-btn' disabled={counter <= 0} onClick={() => {
+                setCounter(counter - 1)
+                }}><RemoveIcon /></Button>}
+
+              </ButtonGroup>
+            </Box>
+
+            <Box style={{alignItems: 'center'}}>
+              <Typography className='quantity-name' >Reps</Typography>
+              <ButtonGroup size="large" aria-label="small outlined button group">
+
+                <Button className='quantity-btn'  onClick={()=> 
+                {setCounterRep(counterRep + 1)}}><AddIcon /></Button>
+
+                {<Box style={{width: '50px', display: 'flex', justifyContent: 'center', alignItems: 'center', paddingTop: '5px'}} className='quantity-counter' >{counterRep}</Box>}
+
+                {<Button className='quantity-btn' disabled={counterRep <= 0} onClick={() => {
+                setCounterRep(counterRep - 1)
+                }}><RemoveIcon /></Button>}
+
+              </ButtonGroup>
+            </Box>
+
+          </Stack>
+      </Stack>
+
+    </Stack>
+    )
+}
+
+export default Profile;
