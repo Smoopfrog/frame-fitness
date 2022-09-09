b79d93c08b0560d388158305903bf7b77295a317
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-09-09 13:54:33 -0700

    pull origin main

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { Box } from '@mui/system'
 import { HeroBanner } from '../components/HeroBanner'
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import CategoriesBar from '../components/CategoriesBar'
 import Exercises from '../components/Exercises'
 import SearchBar from '../components/SearchBar'
@@ -13,11 +13,10 @@ const Home = () => {
   return (
     <Box>
       <HeroBanner />
-      <SearchBar setExercises={setExercises}/>
-    <CategoriesBar category={category} setCategory={setCategory} setExercises={setExercises}/>
-    <div style={{height: "400px"}}></div>
-    <Exercises category={category} exercises={exercises} setExercises={setExercises} />
-    
+      <SearchBar setExercises={setExercises} />
+      <CategoriesBar category={category} setCategory={setCategory} setExercises={setExercises} />
+      {/* <div style={{ height: "400px" }}></div> */}
+      <Exercises category={category} exercises={exercises} setExercises={setExercises} />
     </Box>
   )
 }
