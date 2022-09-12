f7e530ee10fe7bf3a5153d8f5287d40b218afb1a
Live session user <jeandrev1414@gmail.com>
2022-09-11 21:46:59 -0600

    Require db credentials from env file

--- server/db/db.js
+++ server/db/db.js
@@ -2,11 +2,11 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  database: "frame",
-  host: "localhost",
-  user: "jeandre",
-  password: "postgres",
-  port: 5432
+  database: process.env.DATABASE_NAME,
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  port: process.env.DATABASE_PORT
 });
 
 module.exports = pool;
